490aa21e12a520db7f664ba5ba1f76e8dcafe039
thesergioliveira <thesergioliveira@gmail.com>
2021-05-17 15:58:58 +0200

    adding fetch function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,29 @@
 // Any code you will write for your website (in the future!) should go here
+function fetchData() {
+  // this is a GET req
+  fetch("https://opentdb.com/api.php?amount=20&difficulty=easy&type=multiple")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      let userCard = "<h2>ToDos API response </h2>";
+      for (var i = 0; i < data.length; i++) {
+        answers = [];
+        for (prop in data[i]) {
+          // ...add an html radio button
+          userCard +=
+            "<label>" +
+            '<input type="radio" name="question' +
+            i +
+            '" value="' +
+            prop +
+            '">' +
+            prop +
+            ": " +
+            data[i].correct_answer +
+            "</label>";
+        }
+      }
+      document.querySelector(".result").innerHTML = userCard;
+    })
+    .catch((err) => console.log(`So this is what happened ${err}`));
+}
