eea6d25197844a9c72d8f1e225988543c91a8530
meriemkhadraoui <miyakhad@gmail.com>
2021-05-18 11:38:32 +0200

    fetch Data

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,72 +3,36 @@ function fetchEasyData() {
   fetch("https://opentdb.com/api.php?amount=20&difficulty=easy&type=multiple")
     .then((response) => response.json())
     .then((data) => {
-      //console.log(data);
       let userCard1 = "<h2>The question </h2>";
       for (props in data) {
-        for (let i = 0; i < data[props].length; i++) {
-          //console.log(data[props][i]);
+        let questNum = data[props].length / 4;
+        for (let i = 0; i < questNum; i++) {
           let data2 = data[props][i];
+          let answers = [];
           for (props2 in data2) {
-            console.log(`${props2} : ${data2[props2]}`);
-            //console.log(data2["correct_answer"]);
           }
-          userCard1 += `<div><h2>${data2["difficulty"]}</h2></div><li> <div>${data2["question"]}</div> <br>
-          <div>${data2["incorrect_answers"]},${data2["correct_answer"]}</div>    
+          answers = data2["incorrect_answers"].concat(data2["correct_answer"]);
+          // console.log(answers);
+          userCard1 += `<div><h2>${data2["difficulty"]}</h2><h3></div><li> <div>${data2["question"]}</h3></div> <br>
           </li>`;
-        }
-      }
-      document.querySelector(".result").innerHTML = userCard1;
-    })
-    .catch((err) => console.log(`So this is what happened ${err}`));
-}
-// <input type='radio'value= "${data2["incorrect_answers"]}">
-function fetchMediData() {
-  // this is a GET req
-  fetch("https://opentdb.com/api.php?amount=20&difficulty=medium&type=multiple")
-    .then((response) => response.json())
-    .then((data) => {
-      //console.log(data);
-      let userCard2 = "<h2>The question </h2>";
-      for (props in data) {
-        for (let i = 0; i < data[props].length; i++) {
-          //console.log(data[props][i]);
-          let data2 = data[props][i];
-          for (props2 in data2) {
-            console.log(`${props2} : ${data2[props2]}`);
-            //console.log(data2["correct_answer"]);
+          for (let i = 0; i < answers.length; i++) {
+            userCard1 += `<h4>* ${answers[i]}   <input type='radio' name='question' value= "${answers[i]} " ></input></h4> <br>`;
+            // // if answer is correct
+            // userAnswer = ` ${answers[i]}<input type='radio' name='question' value= "${answers[i]}" ></input>: checked`;
+            // if (userAnswer === data2["correct_answer"]) {
+            //   // color the answers green
+            //   userCard1.style.color = "lightgreen";
+            // }
+            // // if answer is wrong or blank
+            // else {
+            //   // color the answers red
+            //   userCard1.style.color = "red";
+            // }
           }
-          userCard2 += `<li><h2><div>${data2["difficulty"]}</h2></div> <br> <div>${data2["question"]}</div> <br>
-            <div>${data2["incorrect_answers"]},${data2["correct_answer"]}</div>    
-            </li>`;
         }
       }
-      document.querySelector(".result").innerHTML = userCard2;
-    })
-    .catch((err) => console.log(`So this is what happened ${err}`));
-}
 
-function fetchDifiData() {
-  // this is a GET req
-  fetch("https://opentdb.com/api.php?amount=20&difficulty=hard&type=multiple")
-    .then((response) => response.json())
-    .then((data) => {
-      //console.log(data);
-      let userCard3 = "<h2>The question </h2>";
-      for (props in data) {
-        for (let i = 0; i < data[props].length; i++) {
-          //console.log(data[props][i]);
-          let data2 = data[props][i];
-          for (props2 in data2) {
-            console.log(`${props2} : ${data2[props2]}`);
-            //console.log(data2["correct_answer"]);
-          }
-          userCard3 += `<li><h2><div>${data2["difficulty"]}</h2></div> <br> <div>${data2["question"]}</div> <br>
-              <div>${data2["incorrect_answers"]},${data2["correct_answer"]}</div>    
-              </li>`;
-        }
-      }
-      document.querySelector(".result").innerHTML = userCard3;
+      document.querySelector(".result").innerHTML = userCard1;
     })
     .catch((err) => console.log(`So this is what happened ${err}`));
 }
