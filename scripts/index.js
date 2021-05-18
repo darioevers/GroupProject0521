05785167a8c42a79d117490e659d4188bab12d38
Dario Evers <darioevers@gmail.com>
2021-05-19 00:55:34 +0200

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,80 @@
-// Any code you will write for your website (in the future!) should go here
+const answerBox = document.querySelector(".answer");
+const question = document.querySelector(".question");
+
+function fetchEasyData() {
+  // this is a GET req
+  fetch("https://opentdb.com/api.php?amount=20&difficulty=easy&type=multiple")
+    .then((response) => response.json())
+    .then((data) => {
+      let userCard1 = "<h2>The easy Level </h2>";
+      for (props in data) {
+        let questNum = data[props].length / 4;
+        for (let i = 0; i < questNum; i++) {
+          let data2 = data[props][i];
+          let answers = [];
+          answers = data2["incorrect_answers"]
+            .concat(data2["correct_answer"])
+            .sort();
+          userCard1 += `<li> <div>${data2["question"]}</h3></div> 
+          </li>`;
+          for (let i = 0; i < answers.length; i++) {
+            userCard1 += `<h4>* ${answers[i]}   <input type='radio' name='question' value= "${answers[i]} " ></input></h4> <br>`;
+            // // if answer is correct
+            // userAnswer = ` ${answers[i]}<input type='radio' name='question' value= "${answers[i]}" ></input>: checked`;
+            // if (userAnswer === data2["correct_answer"]) {
+            //   // color the answers green
+            //   userCard1.style.color = "lightgreen";
+            // }
+            // // if answer is wrong or blank
+            // else {
+            //   // color the answers red
+            //   userCard1.style.color = "red";
+            // }
+          }
+        }
+      }
+
+      question.innerHTML = userCard1;
+    })
+    .catch((err) => console.log(`So this is what happened ${err}`));
+}
+
+// asdasdasasdsadasdsadas
+
+// DOM element selector
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const closeMenu = document.querySelector(".close-menu");
+const navigation = document.querySelector("nav");
+const question = document.querySelector(".question");
+const answer = document.querySelector(".answer");
+const mainContainer = document.querySelector(".main_container");
+const quizMainContainer = document.querySelector(".quiz-main-container");
+const startButton = document.querySelector("#startButton");
+
+const easyQuestions =
+  "https://opentdb.com/api.php?amount=20&difficulty=easy&type=multiple";
+const mediumQuestions =
+  "https://opentdb.com/api.php?amount=20&difficulty=medium&type=multiple";
+const hardQuestions =
+  "https://opentdb.com/api.php?amount=20&difficulty=hard&type=multiple";
+
+const menuClick = () => {
+  hamburgerMenu.style.display = "none";
+  navigation.style.display = "inherit";
+};
+
+hamburgerMenu.addEventListener("click", menuClick);
+closeMenu.addEventListener("click", () => {
+  hamburgerMenu.style.display = "inherit";
+  navigation.style.display = "none";
+});
+
+window.addEventListener(
+  "load",
+  () => (quizMainContainer.style.display = "none")
+);
+
+startButton.addEventListener("click", () => {
+  mainContainer.style.display = "none";
+  quizMainContainer.style.display = "inherit";
+});
