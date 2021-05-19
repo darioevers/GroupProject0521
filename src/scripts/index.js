4e82f1c26447b3cc4ca1218fb07159e922cf8875
meriemkhadraoui <miyakhad@gmail.com>
2021-05-19 18:46:52 +0200

    add Quiz click

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,7 +12,6 @@ const answerBox = document.querySelector(".answer-box");
 const userName = document.querySelector("#userName");
 const playerName = document.querySelector(".player-name");
 const res = document.querySelector(".res");
-
 // URL links for the questions
 const easyQuestions =
   "https://opentdb.com/api.php?amount=20&difficulty=easy&type=multiple";
@@ -145,27 +144,72 @@ function fetchEasyData() {
       }
     })
     .catch((err) => console.log(`So this is what happened ${err}`));
-}
-{
-  /* <p class="answer">${answers[0]}<input type='radio' id="answer" name='question' value= "${answers[0]} " ></input></p>
-          </div>
-          <div class="answer-two">
-            <p class="answer">${answers[1]}<input type='radio' id="answer" name='question' value= "${answers[1]} " ></input></p>
-          </div>
-          <div class="answer-three">
-            <p class="answer">${answers[2]}<input type='radio' id="answer" name='question' value= "${answers[2]} " ></input></p>
-          </div>
-          <div class="answer-four">
-            <p class="answer">${answers[3]}<input type='radio' id="answer" name='question' value= "${answers[3]} " ></input></p>
-          </div>
-           <ul>
-        <li> <span id="answer">${answers[i]}</span></li>
-        <li"><span id="answer">${answers[i - 1]}</span></li>
-        <li><span id="answer">${answers[i - 2]}</span></li>
-        <li> <span id="answer"">${answers[i - 3]}</span></li>
-      </ul> */
+  document.querySelector(".question-card-levl2").style.display = "none";
 }
 
+function fetchDifData() {
+  // this is a GET req
+  fetch("https://opentdb.com/api.php?amount=20&difficulty=hard&type=multiple")
+    .then((response) => response.json())
+    .then((data) => {
+      for (props in data) {
+        let questNum = data[props].length / 4;
+        for (let i = 0; i < questNum; i++) {
+          let data2 = data[props][i];
+          //console.log(data[props][1]["question"]);
+          let answers = [];
+          answers = data2["incorrect_answers"]
+            .concat(data2["correct_answer"])
+            .sort();
+          question.innerHTML = `<div class="question1">${data[props][0]["question"]}</div><br>
+          <div class="question2" style="display: none;">${data[props][1]["question"]}</div><br>
+          <div class="question3" style="display: none;">${data[props][2]["question"]}</div><br>
+          <div class="question4" style="display: none;">${data[props][3]["question"]}</div>`;
+          // console.log(answers);
+          for (let i = 0; i < answers.length; i++) {
+            answerBox.innerHTML = `<div class="answer-one">
+            <p class="answer">  <button>${answers[0]}     </button></p>
+          </div>
+          <div class="answer-two">
+          <p class="answer"> <button>${answers[1]}    </button></p>
+          </div>
+          <div class="answer-three">
+          <p class="answer"> <button>${answers[2]}     </button></p>
+          </div>
+          <div class="answer-four">
+          <p class="answer"> <button>${answers[3]}     </button></p>
+          </div>
+            </div>
+            `;
+          }
+          // Event Bubbling
+          // const list = document.querySelectorAll(".answer-box p");
+          // list.addEventListener("click", function (e) {
+          //   //   console.log(e.target.className);
+          //   if (e.target.className == "answer") {
+          //     let li = e.target.parentElement;
+          //     console.log(li);
+          //     li.style.style.color = "lightgreen";
+          //   }
+          // });
+          // console.log(data2["correct_answer"]);
+          // if (
+          //   document.querySelector("#answer:checked") ===
+          //   data2["correct_answer"]
+          // ) {
+          //   question.
+          // } else {
+          //   question.style.color = "red";
+          // }
+        }
+      }
+    })
+    .catch((err) => console.log(`So this is what happened ${err}`));
+}
+
+function nextLevel() {
+  fetchDifData();
+}
 const hide = document.querySelector("#hide");
 hide.addEventListener("change", () => {
   if (hide.checked) {
