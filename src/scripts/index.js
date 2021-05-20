3eee6d9d7d66ccf8b0376091ef434d9749ae37a4
iulianstan <iulian1226@gmail.com>
2021-05-20 05:50:57 +0200

    Added underline in front of the files from layouts, added some classes in index.html and writed some code in index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,51 +1,9 @@
-const answerBox = document.querySelector(".answer");
-const question = document.querySelector(".question");
-
-function fetchEasyData() {
-  // this is a GET req
-  fetch("https://opentdb.com/api.php?amount=20&difficulty=easy&type=multiple")
-    .then((response) => response.json())
-    .then((data) => {
-      let userCard1 = "<h2>The easy Level </h2>";
-      for (props in data) {
-        let questNum = data[props].length / 4;
-        for (let i = 0; i < questNum; i++) {
-          let data2 = data[props][i];
-          let answers = [];
-          answers = data2["incorrect_answers"]
-            .concat(data2["correct_answer"])
-            .sort();
-          userCard1 += `<li> <div>${data2["question"]}</h3></div> 
-          </li>`;
-          for (let i = 0; i < answers.length; i++) {
-            userCard1 += `<h4>* ${answers[i]}   <input type='radio' name='question' value= "${answers[i]} " ></input></h4> <br>`;
-            // // if answer is correct
-            // userAnswer = ` ${answers[i]}<input type='radio' name='question' value= "${answers[i]}" ></input>: checked`;
-            // if (userAnswer === data2["correct_answer"]) {
-            //   // color the answers green
-            //   userCard1.style.color = "lightgreen";
-            // }
-            // // if answer is wrong or blank
-            // else {
-            //   // color the answers red
-            //   userCard1.style.color = "red";
-            // }
-          }
-        }
-      }
-
-      question.innerHTML = userCard1;
-    })
-    .catch((err) => console.log(`So this is what happened ${err}`));
-}
-
-
-
 // DOM ELEMENT SELECTORS
+
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const closeMenu = document.querySelector(".close-menu");
 const navigation = document.querySelector("nav");
-// const question = document.querySelector(".question");
+
 const answer = document.querySelector(".answer");
 const mainContainer = document.querySelector(".main_container");
 const quizContainer = document.querySelector(".quiz-container");
@@ -54,20 +12,31 @@ const hamburgerMenuIcon = document.querySelector(".hamburger-menu-icon");
 const username = document.querySelector(".username-field");
 const play = document.querySelector(".login-button");
 const loginContainer = document.querySelector(".login-container");
-const  welcomeMessage = document.querySelector(".welcome-message");
+const welcomeMessage = document.querySelector(".welcome-message");
 const userNav = document.querySelector(".username");
 const start = document.querySelector(".start-quiz");
 const mainMenuContainer = document.querySelector(".main-menu-container");
 const homeButton = document.querySelector(".home-button");
 
+const aboutContainer = document.querySelector(".about-container");
+const faqContainer = document.querySelector(".faq-container");
+const leaderboardContainer = document.querySelector(".leaderboard-container");
+
 const easyQuestions =
-  "https://opentdb.com/api.php?amount=20&difficulty=easy&type=multiple";
+  "https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple";
 const mediumQuestions =
-  "https://opentdb.com/api.php?amount=20&difficulty=medium&type=multiple";
+  "https://opentdb.com/api.php?amount=1&difficulty=medium&type=multiple";
 const hardQuestions =
-  "https://opentdb.com/api.php?amount=20&difficulty=hard&type=multiple";
+  "https://opentdb.com/api.php?amount=1&difficulty=hard&type=multiple";
 
-
+const answerBox = document.querySelector(".answer-box");
+const question = document.querySelector(".question");
+const questionBox = document.querySelector(".question-box");
+const nextQuestion = document.querySelector(".next-question");
+const counter = document.querySelector(".counter");
+const aboutButton = document.querySelector(".about-button"); // index.html line 42
+const faqButton = document.querySelector(".faq-button"); // index.html line 47
+const resButton = document.querySelector(".res-button"); // index.html line 52
 
 //FUNCTION FOR OPENING AND CLOSING THE SIDE MENU
 let close = true;
@@ -85,38 +54,91 @@ hamburgerMenuIcon.addEventListener("click", openCloseMenu);
 
 //FUNCTION FOR STORING USERNAME
 let user = "";
-function registerUsername (e){
+function registerUsername(e) {
   console.dir(e.target.value);
   user = e.target.value;
   console.log(user);
 }
 username.addEventListener("keyup", registerUsername);
 
+let correctAnswer;
+
+function fetchEasyData() {
+  // this is a GET req
+  fetch(easyQuestions)
+    .then((res) => res.json())
+    .then((data) => {
+      for (props in data) {
+        let questNum = data[props].length;
+        for (let i = 0; i < questNum; i++) {
+          let data2 = data[props][i];
+
+          let answers = [];
+          answers = data2["incorrect_answers"]
+            .concat(data2["correct_answer"])
+            .sort();
+          correctAnswer = data2["correct_answer"];
+          console.log(data2["correct_answer"]);
+
+          console.log(data2["correct_answer"]);
+          console.log(data2["incorrect_answers"]);
+
+          question.innerHTML = `${data2["question"]}`;
+          answerBox.innerHTML = `
+          <div class="answer-one">
+          <p class="answer">${answers[0]}</p>
+        </div>
+        <div class="answer-two">
+          <p class="answer">${answers[1]}</p>
+        </div>
+        <div class="answer-three">
+          <p class="answer">${answers[2]}</p>
+        </div>
+        <div class="answer-four">
+          <p class="answer">${answers[3]}</p>
+        </div>`;
+        }
+      }
+    })
+    .catch((err) => console.log(`So this is what happened ${err}`));
+}
+
 //FUNCTION FOR DIRECTING GAMER FOR THE WELCOME PAGE
-const startGame = ()=>{
-  console.log(user.lenght);
-  if (user.length < 1){
+
+const startGame = () => {
+  // console.log(user.lenght);
+  if (user.innerHTML < 1) {
     username.setAttribute("placeholder", "Type your name here :)");
   } else {
-    mainMenuContainer.style.display= "block";
+    mainMenuContainer.style.display = "block";
     welcomeMessage.innerHTML = `Welcome, ${user}!`;
     userNav.innerHTML = `${user}`;
-    loginContainer.style.display= "none";
+    loginContainer.style.display = "none";
   }
-}
+};
 play.addEventListener("click", startGame);
 
 //FUNCTION FOR STARTING THE QUIZ
-const startQuiz = ()=> {
-  homeButton.style.display="block";
-  mainMenuContainer.style.display= "none";
-  quizContainer.style.display= "block";
-}
+const startQuiz = () => {
+  homeButton.style.display = "block";
+  mainMenuContainer.style.display = "none";
+  quizContainer.style.display = "block";
+  fetchEasyData();
+};
+
 start.addEventListener("click", startQuiz);
 
-function goHome (){
-  quizContainer.style.display= "none";
-  mainMenuContainer.style.display= "block";
+// Added Event Listener for quiz button from the Navigation bar
+
+const quizNavButton = document.querySelector(".quiz-start"); // Added class quiz-start in index.html/ line 32
+const hallOfFame = document.querySelector(".hallof-fame"); // added hallof-fame class in index.html/ line 37
+const openAboutButton = document.querySelector(".open-about");
+const openFAQButton = document.querySelector(".open-faq");
+const openLeaderboardButton = document.querySelector(".open-leaderboard");
+
+function goHome() {
+  quizContainer.style.display = "none";
+  mainMenuContainer.style.display = "block";
 }
 homeButton.addEventListener("click", goHome);
 // window.addEventListener(
@@ -139,3 +161,59 @@ homeButton.addEventListener("click", goHome);
 //   hamburgerMenu.style.display = "inherit";
 //   navigation.style.display = "none";
 // });
+
+answerBox.addEventListener("click", function (e) {
+  if (e.target.textContent == correctAnswer) {
+    e.target.style.backgroundColor = "green";
+    e.target.style.color = "white";
+    nextQuestion.addEventListener("click", () => {
+      fetchEasyData();
+      let value = counter.textContent;
+      console.log(value);
+      counter.textContent = parseInt(value) + 1;
+    });
+    // e.target.classList.add("correctanswer");
+    // console.log(e.target.textContent);
+    // console.log("You're Good");
+  } else if (e.target.textContent != correctAnswer) {
+    e.target.style.backgroundColor = "red";
+    e.target.style.color = "white";
+    // e.target.classList.add("wronganswer");
+    // console.log("Wrong Answer");
+  }
+});
+
+// MAIN MENU BUTTONS
+// ABOUT
+
+const openAbout = () => {
+  homeButton.style.display = "block";
+  mainMenuContainer.style.display = "none";
+  aboutContainer.style.display = "block";
+};
+openAboutButton.addEventListener("click", openAbout);
+
+// FAQ
+const openFAQ = () => {
+  homeButton.style.display = "block";
+  mainMenuContainer.style.display = "none";
+  faqContainer.style.display = "block";
+};
+openFAQButton.addEventListener("click", openFAQ);
+
+// LEADERBOARD
+const openLeaderboard = () => {
+  homeButton.style.display = "block";
+  mainMenuContainer.style.display = "none";
+  leaderboardContainer.style.display = "block";
+};
+
+const ourLink = "https://darioevers.github.io/GroupProject0521/";
+quizNavButton.addEventListener("click", startQuiz); // Event Listener QUIZ from the Side Menu
+openLeaderboardButton.addEventListener("click", openLeaderboard);
+hallOfFame.addEventListener("click", openLeaderboard); // Event listener for hall of fame button from Menu
+aboutButton.addEventListener("click", openAbout);
+faqButton.addEventListener("click", openFAQ);
+resButton.addEventListener("click", () => ourLink);
+// OR
+resButton.addEventListener("click", html.reload());
