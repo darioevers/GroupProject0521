bb19a80be5ac06772b64ac0002582f5ff988024f
Sergio <thesergioliveira@gmail.com>
2021-06-08 23:15:53 +0200

    updating index.js and fixinng navigations bugs

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,206 @@
-import {startGame, registerUsername, startGameTwo, startQuiz, openAbout, openFAQ, openLeaderboard, openCloseMenu, openQuiz, openingAbout, openFaq, openHall, logOut} from "./navigation.js";
 
-const answerBox = document.querySelector(".answer");
-const questionCard = document.querySelector(".question");
-const nextQuestion = document.querySelector(".next-question");
-const counterCard = document.querySelector(".counter");
-const answerOne = document.querySelector(".answer-one");
-const answerTwo = document.querySelector(".answer-two");
-const answerThree = document.querySelector(".answer-three");
-const answerFour = document.querySelector(".answer-four");
-const answerCard = document.querySelector(".answer-box");
-const userPoints = document.querySelector(".user-points");
-const feedback = document.querySelector(".feedback");
-const reset = document.querySelector(".reset");
+// ============== NAVIGATION FUNCTIONS FOR THE WELCOME PAGE ==============
+// Main page selectors
+const username = document.querySelector(".username-field");
+const hamburgerMenuIcon = document.querySelector(".hamburger-menu-icon");
+const mainMenuContainer = document.querySelector(".main-menu-container");
+const userNav = document.querySelector(".username");
+const loginContainer = document.querySelector(".login-container");
+const play = document.querySelector(".login-button");
+const welcomeMessage = document.querySelector(".welcome-message");
+
+
+// ============== Functions ==============
+
+//registering the user name
+let user = "";
+function registerUsername(e) {
+  console.dir(e.target.value);
+  user = e.target.value;
+  console.log(user);
+}
+
+//function to hide the welcome page and display the main page upon clicking the button "Play"
+const startGame = () => {
+  if (user.length < 1) {
+    username.setAttribute("placeholder", "Type your name here :)");
+  } else {
+    loginContainer.style.display = "none";
+    hamburgerMenuIcon.style.display = "block";
+    mainMenuContainer.style.display = "block";
+    welcomeMessage.innerHTML = `Welcome, ${user}!`;
+    userNav.innerHTML = `${user}`;
+  }
+}; 
+
+
+//function to hide the welcome page and display the main page upon pressing the key "Enter"
+const startGameTwo = (e)=>{
+    if (user == ""){
+      username.setAttribute("placeholder", "Type your name here :)");
+    } else if (event.key == "Enter"){
+      loginContainer.style.display= "none";
+      hamburgerMenuIcon.style.display = "block";
+      mainMenuContainer.style.display= "block";
+      welcomeMessage.innerHTML = `Welcome, ${user}!`;
+      userNav.innerHTML = `${user}`;
+    }
+  }
+
+// ============== Event listeners for the welcome page ==============
+  
+//event listener for the play button 
+play.addEventListener("click", startGame);
+//event listener for the text input  
+username.addEventListener("keypress", startGameTwo);
+username.addEventListener("keyup", registerUsername); 
+
+// ============== NAVIGATION FUNCTIONS FOR THE MAIN PAGE ==============
+
+//============== Main page selectors ==============
+const homeButton = document.querySelector(".home-button");
+const quizContainer = document.querySelector(".quiz-container");
+const start = document.querySelector(".start-quiz");
+const openAboutButton = document.querySelector(".open-about");
+const openFAQButton = document.querySelector(".open-faq");
+const openLeaderboardButton = document.querySelector(".open-leaderboard");
+const faqContainer = document.querySelector(".faq-container");
+const aboutContainer = document.querySelector(".about-container");
+const leaderboardContainer = document.querySelector(".leaderboard-container");
+
+//============== Functions ==============
+
+//function to initiate the quiz
+const startQuiz = () => {
+  hamburgerMenuIcon.style.display = "block";
+  homeButton.style.display = "block";
+  mainMenuContainer.style.display = "none";
+  quizContainer.style.display = "block";
+  // fecthEasyQuestions();
+};
+
+//function to display the about page
+const openAbout = () => {
+  hamburgerMenuIcon.style.display = "block";
+  homeButton.style.display = "block";
+  mainMenuContainer.style.display = "none";
+  aboutContainer.style.display = "block";
+};
+
+//function to display the FAQ page
+const openFAQ = () => {
+  hamburgerMenuIcon.style.display = "block";
+  homeButton.style.display = "block";
+  mainMenuContainer.style.display = "none";
+  faqContainer.style.display = "block";
+};
+
+//function to display the leaderboard page
+const openLeaderboard = () => {
+  hamburgerMenuIcon.style.display = "block";
+  homeButton.style.display = "block";
+  mainMenuContainer.style.display = "none";
+  leaderboardContainer.style.display = "block";
+};
+
+// ============== Even Listeners for the main page ==============
+
+//event listener for the quiz "start" button
+start.addEventListener("click", startQuiz);
+//event listener for the about "click" button
+openAboutButton.addEventListener("click", openAbout);
+//event listener for the FAQ "click" button 
+openFAQButton.addEventListener("click", openFAQ);
+//event listener for the leaderboard "click" button
+openLeaderboardButton.addEventListener("click", openLeaderboard);
+
+// ============== NAVIGATION FUNCTIONS FOR THE SIDE MENU ==============
+
+// ============== Side menu selectors ==============
+const navigation = document.querySelector("nav");
+const restart = document.querySelector(".reload");
+const about = document.querySelector(".about");
+const hallOfFame = document.querySelector(".hallOfFame");
+const faq = document.querySelector(".faq");
+const quiz = document.querySelector(".quiz");
+
+//============== Functions ==============
+
+//Function to display and hide the side menu
+let close = true;
+const openCloseMenu = () => {
+  if (close) {
+    hamburgerMenuIcon.classList.toggle("open");
+    navigation.style.display = "block";
+  } else {
+    hamburgerMenuIcon.classList.toggle("open");
+    navigation.style.display = "none";
+  }
+  close = !close;
+};
+
+//Function for the quiz list item on the side menu
+ function openQuiz (){
+    mainMenuContainer.style.display = "none";
+    leaderboardContainer.style.display = "none";
+    aboutContainer.style.display = "none";
+    faqContainer.style.display = "none";
+    quizContainer.style.display = "block";
+    loginContainer.style.display = "none";
+    openCloseMenu();
+ }
+//Function for the hall of fame list item on the side menu
+function openHall (){
+    mainMenuContainer.style.display = "none";
+    leaderboardContainer.style.display = "block";
+    aboutContainer.style.display = "none";
+    faqContainer.style.display = "none";
+    quizContainer.style.display = "none";
+    loginContainer.style.display = "none";
+    openCloseMenu();
+ }
+//Function for the about list item on the side menu
+function openingAbout (){
+    mainMenuContainer.style.display = "none";
+    leaderboardContainer.style.display = "none";
+    aboutContainer.style.display = "block";
+    faqContainer.style.display = "none";
+    quizContainer.style.display = "none";
+    loginContainer.style.display = "none";
+    openCloseMenu();
+ }
+
+ //Function for the FAQ list item on the side menu
+ function openFaq (){
+    mainMenuContainer.style.display = "none";
+    leaderboardContainer.style.display = "none";
+    aboutContainer.style.display = "none";
+    faqContainer.style.display = "block";
+    quizContainer.style.display = "none";
+    loginContainer.style.display = "none";
+    openCloseMenu();
+ }
+
+//Function for the logout list item on the side menu
+function logOut (){
+    setTimeout("location.reload(true);", 0.5);
+  }
+ 
+// ============== Event listeners for the side menu ==============
+//Event listener on the humburger menu div
+hamburgerMenuIcon.addEventListener("click", openCloseMenu);
+//Event listener on the quiz item
+quiz.addEventListener("click", openQuiz);
+//Event listener on the about item
+about.addEventListener("click", openingAbout);
+//Event listener on the hall of fame item
+hallOfFame.addEventListener("click", openHall);
+//Event listener on the FAQ item
+faq.addEventListener("click", openFaq);
+//Event listener on the logout item
+restart.addEventListener("click", logOut);
+
+// ============== END OF NAVIGATION FUNCTIONS ==============
 
 const easyQuestions =
   "https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple";
@@ -81,6 +270,7 @@ function fecthEasyQuestions() {
       leaderboardContainer.style.diplay = "block";
     }
   }
+const nextQuestion = document.querySelector(".next-question");  
 nextQuestion.addEventListener("click", fecthEasyQuestions);
 
 let points = 0;
@@ -105,41 +295,15 @@ function calculating (e){
   }
   // e.target.style.backgroundColor= "white";
 }
+const answerCard = document.querySelector(".answer-box");
 answerCard.addEventListener("click", calculating);
 
-// DOM ELEMENT SELECTORS FOR NAVIGATIONS
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-const closeMenu = document.querySelector(".close-menu");
-const navigation = document.querySelector("nav");
-const question = document.querySelector(".question");
-const answer = document.querySelector(".answer");
-const loginContainer = document.querySelector(".login-container");
-const mainMenuContainer = document.querySelector(".main-menu-container");
-const quizContainer = document.querySelector(".quiz-container");
-const aboutContainer = document.querySelector(".about-container");
-const faqContainer = document.querySelector(".faq-container");
-const leaderboardContainer = document.querySelector(".leaderboard-container");
-const finalContainer = document.querySelector(".final-container");
-const startButton = document.querySelector("#startButton");
-const hamburgerMenuIcon = document.querySelector(".hamburger-menu-icon");
-const username = document.querySelector(".username-field");
-const play = document.querySelector(".login-button");
-const printButton = document.querySelector(".printbutton");
-const openAboutButton = document.querySelector(".open-about");
-const openFAQButton = document.querySelector(".open-faq");
-const openLeaderboardButton = document.querySelector(".open-leaderboard");
-const mainContainer = document.querySelector(".main_container"); 
-const welcomeMessage = document.querySelector(".welcome-message");
-const userNav = document.querySelector(".username");
-const start = document.querySelector(".start-quiz");
-const homeButton = document.querySelector(".home-button");
-const restart = document.querySelector(".reload");
-const themeButton = document.querySelector(".theme-button");
 
 // LIGHT DARK MODE
 const toggleTheme = () => {
   document.body.classList.toggle("dark-theme");
 };
+const themeButton = document.querySelector(".theme-button");
 themeButton.addEventListener("click", toggleTheme);
 
 function goHome() {
@@ -152,53 +316,11 @@ function goHome() {
 }
 homeButton.addEventListener("click", goHome);
 
-//FUNCTION FOR THE THE LOGOUT ITEM ON THE MENU
-
-
-
-// LEADERBOARD
-// const openLeaderboard = () => {
-//   homeButton.style.display = "block";
-//   leaderboardContainer.style.display = "block";
-//   mainMenuContainer.style.display = "none";
-//   finalContainer.style.display = "none";
-// };
-
-// openLeaderboardButton.addEventListener("click", openLeaderboard);
-
-
-
 // ADDITIONAL FUNCTIONS
 // PRINT PAGE
-
 const printPage = () => {
   window.print();
 };
+const printButton = document.querySelector(".printbutton");
 printButton.addEventListener("click", printPage);
 
-// window.addEventListener(
-//   "load",
-//   () => (quizMainContainer.style.display = "none")
-// );
-// window.addEventListener(
-//   "
-//   () => (quizMainContainer.style.display = "none")
-// );
-
-// startButton.addEventListener("click", () => {
-//   mainContainer.style.display = "none";
-//   quizMainContainer.style.display = "inherit";
-// });
-
-// const menuClick = () => {
-//   hamburgerMenu.style.display = "none";
-//   navigation.style.display = "inherit";
-// };
-
-// hamburgerMenu.addEventListener("click", menuClick);
-// closeMenu.addEventListener("click", () => {
-//   hamburgerMenu.style.display = "inherit";
-//   navigation.style.display = "none";
-// });
-
-export default fecthEasyQuestions;
--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -1,204 +1,34 @@
-import fecthEasyQuestions from "./index.js";
-// ============== DOM SELECTORS ==============
-
-//Main page selectors
-const homeButton = document.querySelector(".home-button");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const closeMenu = document.querySelector(".close-menu");
+const navigation = document.querySelector("nav");
+const question = document.querySelector(".question");
+const answer = document.querySelector(".answer");
 const quizContainer = document.querySelector(".quiz-container");
-const start = document.querySelector(".start-quiz");
+const aboutContainer = document.querySelector(".about-container");
+const faqContainer = document.querySelector(".faq-container");
+const leaderboardContainer = document.querySelector(".leaderboard-container");
+const finalContainer = document.querySelector(".final-container");
+const startButton = document.querySelector("#startButton");
+
 const openAboutButton = document.querySelector(".open-about");
 const openFAQButton = document.querySelector(".open-faq");
 const openLeaderboardButton = document.querySelector(".open-leaderboard");
-const faqContainer = document.querySelector(".faq-container");
-const aboutContainer = document.querySelector(".about-container");
-const leaderboardContainer = document.querySelector(".leaderboard-container");
+const mainContainer = document.querySelector(".main_container"); 
 
-//Side menu selectors
-const hamburgerMenuIcon = document.querySelector(".hamburger-menu-icon");
-const navigation = document.querySelector("nav");
+const start = document.querySelector(".start-quiz");
+const homeButton = document.querySelector(".home-button");
 const restart = document.querySelector(".reload");
-const about = document.querySelector(".about");
-const hallOfFame = document.querySelector(".hallOfFame");
-const faq = document.querySelector(".faq");
-const quiz = document.querySelector(".quiz");
-console.log(faq);
+const answerBox = document.querySelector(".answer");
+const questionCard = document.querySelector(".question");
+
+const counterCard = document.querySelector(".counter");
+const answerOne = document.querySelector(".answer-one");
+const answerTwo = document.querySelector(".answer-two");
+const answerThree = document.querySelector(".answer-three");
+const answerFour = document.querySelector(".answer-four");
+const userPoints = document.querySelector(".user-points");
+const feedback = document.querySelector(".feedback");
+const reset = document.querySelector(".reset");
 
 
-// ============== Navigation functions for the welcome page ==============
 
-const play = document.querySelector(".login-button");
-//registering the user name
-let user = "";
-function registerUsername(e) {
-  console.dir(e.target.value);
-  user = e.target.value;
-  console.log(user);
-}
-
-//function to hide the welcome page and display the main page upon clicking the button "Play"
-const startGame = () => {
-  const username = document.querySelector(".username-field");
-  const hamburgerMenuIcon = document.querySelector(".hamburger-menu-icon");
-  const mainMenuContainer = document.querySelector(".main-menu-container");
-  const userNav = document.querySelector(".username");
-  const loginContainer = document.querySelector(".login-container");
-    if (user.length < 1) {
-      username.setAttribute("placeholder", "Type your name here :)");
-    } else {
-      hamburgerMenuIcon.style.display = "block";
-      mainMenuContainer.style.display = "block";
-      welcomeMessage.innerHTML = `Welcome, ${user}!`;
-      userNav.innerHTML = `${user}`;
-      loginContainer.style.display = "none";
-    }
-  }; 
-
-//function to hide the welcome page and display the main page upon pressing the key "Enter"
-const startGameTwo = (e)=>{
-  const username = document.querySelector(".username-field");
-  const hamburgerMenuIcon = document.querySelector(".hamburger-menu-icon");
-  const welcomeMessage = document.querySelector(".welcome-message");
-  const userNav = document.querySelector(".username");
-   const loginContainer = document.querySelector(".login-container");
-    if (user == ""){
-      username.setAttribute("placeholder", "Type your name here :)");
-    } else if (event.key == "Enter"){
-      hamburgerMenuIcon.style.display = "block";
-      mainMenuContainer.style.display= "block";
-      welcomeMessage.innerHTML = `Welcome, ${user}!`;
-      userNav.innerHTML = `${user}`;
-      loginContainer.style.display= "none";
-    } 
-  }
-
-    //Event listeners for the welcome page
-  
-//event listener for the play button 
-play.addEventListener("click", startGame);
-//event listeners for the text input  
-username.addEventListener("keypress", startGameTwo);
-username.addEventListener("keyup", registerUsername);
-
-// ============== Navigation functions for the main page ==============
-
-//function to initiate the quiz
-const startQuiz = () => {
-    hamburgerMenuIcon.style.display = "block";
-    homeButton.style.display = "block";
-    mainMenuContainer.style.display = "none";
-    quizContainer.style.display = "block";
-    fecthEasyQuestions();
-  };
-
-  //function to display the about page
-const openAbout = () => {
-    hamburgerMenuIcon.style.display = "block";
-    homeButton.style.display = "block";
-    mainMenuContainer.style.display = "none";
-    aboutContainer.style.display = "block";
-  };
-
-//function to display the FAQ page
-const openFAQ = () => {
-    hamburgerMenuIcon.style.display = "block";
-    homeButton.style.display = "block";
-    mainMenuContainer.style.display = "none";
-    faqContainer.style.display = "block";
-  };
-
-//function to display the leaderboard page
-const openLeaderboard = () => {
-    hamburgerMenuIcon.style.display = "block";
-    homeButton.style.display = "block";
-    mainMenuContainer.style.display = "none";
-    leaderboardContainer.style.display = "block";
-  };
-
-    //Even Listeners for the main page
-
-//event listener for the quiz "start" button
-start.addEventListener("click", startQuiz);
-//event listener for the about "click" button
-openAboutButton.addEventListener("click", openAbout);
-//event listener for the FAQ "click" button 
-openFAQButton.addEventListener("click", openFAQ);
-//event listener for the leaderboard "click" button
-openLeaderboardButton.addEventListener("click", openLeaderboard);
-
-// ============== Navigation functions for the side menu ==============
-
-//Fucntion to display and hide the side menu
-let close = true;
-const openCloseMenu = () => {
-  if (close) {
-    hamburgerMenuIcon.classList.toggle("open");
-    navigation.style.display = "block";
-  } else {
-    hamburgerMenuIcon.classList.toggle("open");
-    navigation.style.display = "none";
-  }
-  close = !close;
-};
-
-//Function for the quiz list item on the side menu
- function openQuiz (){
-    mainMenuContainer.style.display = "none";
-    leaderboardContainer.style.display = "none";
-    aboutContainer.style.display = "none";
-    faqContainer.style.display = "none";
-    quizContainer.style.display = "block";
-    loginContainer.style.display = "none";
-    openCloseMenu();
- }
-//Function for the hall of fame list item on the side menu
-function openHall (){
-    mainMenuContainer.style.display = "none";
-    leaderboardContainer.style.display = "block";
-    aboutContainer.style.display = "none";
-    faqContainer.style.display = "none";
-    quizContainer.style.display = "none";
-    loginContainer.style.display = "none";
-    openCloseMenu();
- }
-//Function for the about list item on the side menu
-function openingAbout (){
-    mainMenuContainer.style.display = "none";
-    leaderboardContainer.style.display = "none";
-    aboutContainer.style.display = "block";
-    faqContainer.style.display = "none";
-    quizContainer.style.display = "none";
-    loginContainer.style.display = "none";
-    openCloseMenu();
- }
-
- //Function for the FAQ list item on the side menu
- function openFaq (){
-    mainMenuContainer.style.display = "none";
-    leaderboardContainer.style.display = "none";
-    aboutContainer.style.display = "none";
-    faqContainer.style.display = "block";
-    quizContainer.style.display = "none";
-    loginContainer.style.display = "none";
-    openCloseMenu();
- }
-
-//Function for the logout list item on the side menu
-function logOut (){
-    setTimeout("location.reload(true);", 0.5);
-  }
- 
-    //Event listeners for the side menu
-//Event listener on the humburger menu div
-hamburgerMenuIcon.addEventListener("click", openCloseMenu);
-//Event listener on the quiz item
-quiz.addEventListener("click", openQuiz);
-//Event listener on the about item
-about.addEventListener("click", openingAbout);
-//Event listener on the hall of fame item
-addEventListener("click", openHall);
-//Event listener on the FAQ item
-faq.addEventListener("click", openFaq);
-//Event listener on the logout item
-restart.addEventListener("click", logOut);
-
-
-export { startGame, registerUsername, startGameTwo, startQuiz, openAbout, openFAQ, openLeaderboard, openCloseMenu, openQuiz, openingAbout, openFaq, openHall, logOut};
